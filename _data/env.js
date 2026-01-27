e2154ddf71a6ea9f6195687164aafcb2870662b4
dreamhouseweb55-web <dreamhouseweb55@gmail.com>
2026-01-27 07:46:42 +0200

    Refactor: Centralize env vars in .env and use dotenv

--- _data/env.js
+++ _data/env.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 module.exports = function () {
     return {
-        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || "AIzaSyDSifLl1nVaD5NMlfXQ6ovQmmBr4aKazBY",
-        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || "dreamhouse-94ca7.firebaseapp.com",
-        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || "dreamhouse-94ca7",
-        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET || "dreamhouse-94ca7.firebasestorage.app",
-        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID || "989701311407",
-        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || "1:989701311407:web:425f5b715a9e86e19e3d5a",
-        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID || "G-LWCM2NHCQ3",
-        ALLOWED_EMAIL: process.env.ALLOWED_EMAIL || "dreamhouseweb55@gmail.com"
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+        ALLOWED_EMAIL: process.env.ALLOWED_EMAIL
     };
 };
